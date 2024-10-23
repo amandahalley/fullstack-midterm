80c20a692e6b570d1d8c89cf7bc5c731a2cd0029
Tyler Wall <Tyler.wall@keyin.com>
2024-10-23 17:10:40 -0230

    test - Tyler Wall

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const express = require('express');
-const path = require('path');
-const { getTopRatedMovies, getMoviesByGenre, getMovieDetailsById, selectRandomMovieId } = require('./utils/movieUtils');
-const { Movies, Genres } = require('./utils/data');
+const express = require("express");
+const path = require("path");
+const {
+  getTopRatedMovies,
+  getMoviesByGenre,
+  getMovieDetailsById,
+  selectRandomMovieId,
+} = require("./utils/movieUtils");
+const { Movies, Genres } = require("./utils/data");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
-app.get('/', (request, response) => {
-    response.render('index');
+app.get("/", (request, response) => {
+  response.render("index");
 });
 
-app.get('/movie/:id', (request, response) => {
-    //For use with links like: /movie/1
-    const movieId = request.params.id;
+app.get("/movie/:id", (request, response) => {
+  //For use with links like: /movie/1
+  const movieId = request.params.id;
 });
 
 //Add remaining routes here
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
+
+// test
