ae5fd04ebbd43c49756cf20523daa7a26d6ce39a
ahllyy <amanda.halley@keyin.com>
2024-10-29 18:21:18 -0230

    added routes for random movie page, upcoming movies page, and top rated movies page

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ app.get("/", (request, response) => {
   response.render("index");
 });
 
+//Added required pages
+app.get('/movie', (req, res) => {
+  res.render('movie');
+})
+
+app.get('/upcoming', (req, res) => {
+  res.render('upcoming');
+})
+
+app.get('/topRated', (req, res) => {
+  res.render('topRated');
+})
+
 app.get("/movie/:id", (request, response) => {
   //For use with links like: /movie/1
   const movieId = request.params.id;
