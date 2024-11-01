0d5cb62228cd9ff5f2a6bb7e4b0cfa2bf0956619
Tyler Wall <Tyler.wall@keyin.com>
2024-11-01 17:33:36 -0230

    footer icon updates

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const {
 const { Movies, Genres } = require("./utils/data");
 
 const app = express();
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -45,14 +46,15 @@ app.get("/movie", (req, res) => {
 
 // Top-rated movies route: Display the top 15 movies by rating
 app.get("/topRated", (req, res) => {
-  const topRatedMovies = getTopRatedMovies(15);  
+  const topRatedMovies = getTopRatedMovies(15);
   res.render("topRated", { movies: topRatedMovies });
 });
 
 // Upcoming movies route: Display upcoming movies
 app.get("/upcoming", (req, res) => {
-  const currentYear = new Date().getFullYear(); //Gets current year 
-  const upcomingMovies = Movies.filter( //Retrieves movies with release year greater then current
+  const currentYear = new Date().getFullYear(); //Gets current year
+  const upcomingMovies = Movies.filter(
+    //Retrieves movies with release year greater then current
     (movie) => movie.releaseYear > currentYear
   ).slice(0, 5);
   res.render("upcoming", { movies: upcomingMovies }); //pass list of upcoming movies to template
@@ -66,7 +68,7 @@ app.get("/movies/genre/:genre", (req, res) => {
   //Checks for movies found for requested genre genre
   //If found, renders templates with genre and list of movies
   //Otherwise returns error message
-  if (moviesByGenre.length > 0) { 
+  if (moviesByGenre.length > 0) {
     res.render("moviesByGenre", { genre, movies: moviesByGenre });
   } else {
     res.status(404).send("No movies found for the specified genre.");
